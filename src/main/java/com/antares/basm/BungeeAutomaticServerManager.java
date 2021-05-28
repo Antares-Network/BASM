b45f8963c9ec7ec6ef2827812d8f73cf631f5441
piotrwyrw <pw.piotr.wyrwas@gmail.com>
2021-05-28 10:51:06 +0200

    Create maven project

---
+++ src/main/java/com/antares/basm/BungeeAutomaticServerManager.java
@@ -0,0 +1,17 @@
+package com.antares.basm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BungeeAutomaticServerManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
