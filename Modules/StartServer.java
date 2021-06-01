2e0dc39c481bbe8bb60bf13cee49c7b5f797f91f
Nate Goldsborough <business@playantares.com>
2021-06-01 16:49:12 -0500

    delete StartServer

--- Modules/StartServer.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class StartServer {
-    public void StartServer(String name) throws IOException {
-
-        System.getProperty("user.dir");
-        Runtime.getRuntime().exec("../../" + name + "/start.sh");
-    }
-}
