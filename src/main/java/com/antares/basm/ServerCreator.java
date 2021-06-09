7921fea243747082d265de83fc8c974d9496b23b
Nate Goldsborough <business@playantares.com>
2021-06-09 17:18:29 -0500

    Start work on server_dictionary

--- src/main/java/com/antares/basm/ServerCreator.java
+++ src/main/java/com/antares/basm/ServerCreator.java
@@ -1,11 +1,17 @@
 package com.antares.basm;
+
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ServerCreator {
 
@@ -31,16 +37,35 @@ public class ServerCreator {
         StateMessage[] messages = new StateMessage[2];
         messages[0] = ServerHelper.addServer(info);
         if (messages[0].state == State.ERROR) {
-            return new StateMessage[] {messages[0]};
+            return new StateMessage[] { messages[0] };
         }
         try {
-            ServerFileManager result = new ServerFileManager(player.getName(), String.valueOf(info.getAddress().getPort()), player.getUniqueId());
+            ServerFileManager result = new ServerFileManager(player.getName(),
+                    String.valueOf(info.getAddress().getPort()), player.getUniqueId());
         } catch (IOException e) {
             e.printStackTrace();
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
         messages[1] = ServerHelper.addServerToConfig(info);
+
+        try {
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get(basm.getProxy().getPluginsFolder() + "/server_dictionary.json"));
+            Map<String, Object> server = new HashMap<>();
+            server.put("id", 1);
+            server.put("owner", player.getName());
+            server.put("name", player.getName());
+            server.put("nickname", "");
+            server.put("UUID", player.getUniqueId());
+            server.put("discord", "");
+            ObjectMapper mapper = new ObjectMapper();
+            writer.write(mapper.writeValueAsString(server));
+            writer.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
         return messages;
     }
 
