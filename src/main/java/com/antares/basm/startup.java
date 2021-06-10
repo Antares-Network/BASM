6f34257d3bf69c1844907bb1da0726fd249e409c
Nate Goldsborough <business@playantares.com>
2021-06-10 12:52:17 -0500

    Clean things up

--- src/main/java/com/antares/basm/startup.java
+++ src/main/java/com/antares/basm/startup.java
@@ -34,16 +34,12 @@ public class startup {
     }
 
     public static void serverStart(String name) throws IOException {
-        System.out.println("cd " + new File(".").getAbsolutePath().replace("waterfall/.", name));
-        // "cd " + new File(".").getAbsolutePath().replace("waterfall/.", name)
         ProcessBuilder pb = new ProcessBuilder(new String[] { "java", "-jar",
                 new File(".").getAbsolutePath().replace("waterfall/.", name) + "/paper_latest.jar" });
-        // new String[] { "sh", new File(".").getAbsolutePath().replace("waterfall/.",
-        // name) + "/start.sh" });
         pb.redirectErrorStream(true);
         pb.directory(new File(new File(".").getAbsolutePath().replace("waterfall/.", name)));
         try {
-            Thread.sleep(8000);
+            Thread.sleep(3000);
             pb.start();
         } catch (IOException exp) {
             exp.printStackTrace();
