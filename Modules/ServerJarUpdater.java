e7631d0640bcb5ed2197a0561806764b2c3bfdcf
Nate Goldsborough <nathen418@playantares.com>
2021-07-17 21:27:08 -0500

    Minor updates

--- Modules/ServerJarUpdater.java
+++ Modules/ServerJarUpdater.java
@@ -4,23 +4,47 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ServerJarUpdater {
-
-
-
-    private static void copySourceToDest(String dest, Path fromPath, Path source) {
-        Path destination = Paths.get(dest, source.toString().substring(fromPath.toString().length()));
+    //compare the md5 of two files in different directories
+    public static boolean compareMD5(String file1, String file2) {
         try {
-            Files.copy(source, destination);
-
+            Path path1 = Paths.get(file1);
+            Path path2 = Paths.get(file2);
+            String md51 = Files.readAllLines(path1).get(0);
+            String md52 = Files.readAllLines(path2).get(0);
+            return md51.equals(md52);   //if md5 is the same, return true
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
+    //copy a file with a given name from a source directory to the target directory if the source file exists
+    public static void copyFile(String source, String target, String fileName) {
+        try {
+            Path sourcePath = Paths.get(source, fileName);
+            Path targetPath = Paths.get(target, fileName);
+            if (Files.exists(sourcePath)) {
+                //copy the file and overwrite the target file if it exists
+                Files.copy(sourcePath, targetPath);
+            }else{
+                System.out.println("File " + fileName + " not found in " + source);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws IOException{
-        String name = "nathen418";
-        Path fromPath = Paths.get("latest_jar");
-        Files.walk(fromPath).forEach(source -> copySourceToDest(name, fromPath, source));
+    public static void updateServerJar(String sourceDir, String targetDir, String fileName){
+        if(!compareMD5(sourceDir + fileName, targetDir + fileName)){
+            copyFile(sourceDir, targetDir, fileName);
+        }
+        //check again if the md5 is the same
+        if(!compareMD5(sourceDir + fileName, targetDir + fileName)){
+            System.out.println("MD5 of " + fileName + " is not the same in " + sourceDir + " and " + targetDir);
+            System.out.println("Server jar updating failed for server of name: " + targetDir);
+            return;
+        }
+        System.out.println("Server jar updated sucessfully");
     }
-    
+
 }
